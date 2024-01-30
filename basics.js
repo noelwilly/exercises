5d3cbd16d23bc30ae8fb50fd3318533ea1931112
NoelWilliam <noelwilliamm@gmail.com>
2024-01-30 16:37:22 +0100

    finito

--- basics.js
+++ basics.js
@@ -1,1 +1,30 @@
-console.log("skibidi");
+function randomTal(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function checkNum(n) {
+  if (n === finNum) {
+    return "TILLYKKE! " + n + " ER KORREKT!";
+  } else if (n < finNum) {
+    return "DET ER HØJERE END " + n;
+  } else if (n > finNum) {
+    return "DET ER LAVERE END " + n;
+  }
+}
+
+const finNum = randomTal(100);
+const enterNum = document.getElementById("input");
+const number = document.querySelector(".ranNum");
+
+function parse() {
+  let guess = parseInt(document.querySelector("#input").value);
+  console.log(guess);
+  console.log(checkNum(guess));
+  number.textContent = checkNum(guess);
+}
+
+enterNum.addEventListener("keyup", function () {
+  if (event.keyCode === 13) {
+    parse();
+  }
+});
